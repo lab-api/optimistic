d452136ca05bb1a5d8f25090c64e1907e4149e2f
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-10-08 11:22:14 -0600

    Add new features: pass a set of points to override default sampling method, allow post-actuation delays, measure additional dependent variables during optimization, and allow objective functions to return DataFrames with multiple results

--- optimistic/algorithms/algorithm.py
+++ optimistic/algorithms/algorithm.py
@@ -1,24 +1,76 @@
 import numpy as np
 import pandas as pd
 import attr
+import time
 
 @attr.s
 class Algorithm:
+    ''' A base class for parameter space exploration and optimization.
+
+        Arguments:
+            experiment (callable): a function or method which measures the
+                                   objective function at the current point in
+                                   the parameter space. Takes no positional arguments.
+            parameters (dict): instances of the parametric.Parameter class to use
+                               during optimization. Defaults to empty, and Parameters
+                               can be added by calling Algorithm.add_parameter().
+            bounds (dict): tuple bounds indexed by Parameter names. Defaults to empty,
+                           and is set when adding Parameters.
+            points (dict): optional points for each Parameter to override default
+                           point generation in optimizers, e.g. sampling locations
+                           for a grid search or initial population in a genetic algorithm.
+            delays (dict): post-actuation delay. Set in add_parameter and defaults to zero.
+                           Should be set in Algorithm.add_parameter() if a delay is
+                           desired between actuation and measurement.
+            data (pandas.DataFrame): results of the optimization. Defaults to empty,
+                                     but a previously-generated DataFrame can be
+                                     passed to speed up optimization of a stationary
+                                     objective function.
+            dependent_variables (dict): a labeled set of functions to measure
+                                        along-side the objective function. For
+                                        example, it may be desirable to monitor
+                                        laser power while optimizing the frequency.
+    '''
     experiment = attr.ib()
     parameters = attr.ib(factory=dict)
     bounds = attr.ib(factory=dict)
+    points = attr.ib(factory=dict)        # optional overrides to search points
+    delays = attr.ib(factory=dict)        # post-actuation delays
     data = attr.ib(factory=pd.DataFrame)
+    dependent_variables = attr.ib(factory=dict)
 
-    def add_parameter(self, parameter, bounds=None):
+    def add_parameter(self, parameter, bounds=None, points=None, delay=0):
+        ''' Adds a parameter.
+
+            Arguments:
+                parameter (parametric.Parameter)
+                bounds (tuple): a (min, max) pair defining the extent of the
+                                optimization. If no bounds are passed but a set of
+                                points is specified, use the min/max values of
+                                points; otherwise, use the default parameter bounds.
+                points (array-like): a list of points to override sampling behavior
+                                     in the algorithm.
+                delay (float): optional delay after actuation.
+        '''
         self.parameters[parameter.name] = parameter
         if bounds is None:
-            if parameter.bounds == (-np.inf, np.inf):
+            if points is not None:
+                self.bounds[parameter.name] = (np.min(points), np.max(points))
+            elif parameter.bounds == (-np.inf, np.inf):
                 raise ValueError('Define parameter bounds!')
-            self.bounds[parameter.name] = parameter.bounds
+            else:
+                self.bounds[parameter.name] = parameter.bounds
         else:
             self.bounds[parameter.name] = bounds
 
+        if points is not None:       # override point selection
+            self.points[parameter.name] = points
+
+        self.delays[parameter.name] = delay
         return self
+
+    def add_dependent_variable(self, function):
+        self.dependent_variables.append(function)
 
     def actuate(self, point):
         ''' Sets each parameter to the value corresponding to a normalized value
@@ -32,16 +84,42 @@ class Algorithm:
             if not bounds[0] <= point[i] <= bounds[1]:
                 raise ValueError(f'The optimizer requested a point outside the valid bounds for parameter {parameter.name} and will now terminate.')
             parameter(point[i])
+            time.sleep(self.delays[name])
             i += 1
+
+    def result_to_dataframe(self, result):
+        ''' Takes an experimental result and forms a DataFrame to append to self.data. '''
+        ## case 1: type(result) == float
+        if isinstance(result, float):
+            new_data = pd.DataFrame(index=[len(self.data)], columns=[*list(self.parameters), self.experiment.__name__])
+            for name, parameter in self.parameters.items():
+                new_data[name] = parameter()
+            new_data[self.experiment.__name__] = result
+
+        if isinstance(result, pd.DataFrame):
+            ''' It's expected that self.experiment return a DataFrame with columns
+                corresponding to self.experiment.__name__ and zero or more
+                other variables to be tracked (not contained in self.dependent_variables).
+            '''
+            new_data = result
+            for name, parameter in self.parameters.items():
+                new_data[name] = parameter()
+
+        ## measure dependent variables
+        for function in self.dependent_variables:
+            new_data[function.__name__] = function()
+
+        return new_data
+
 
     def measure(self, point):
         ''' Actuate to specified point and measure result '''
         self.actuate(point)
-        i = len(self.data)
-        for name, parameter in self.parameters.items():
-            self.data.loc[i, name] = parameter()
-        self.data.loc[i, 'result'] = self.experiment()
-        return self.data.loc[i, 'result']
+        new_data = self.result_to_dataframe(self.experiment())
+        self.data = self.data.append(new_data)
+        self.data = self.data.reset_index().drop('index', axis=1)
+
+        return self.data.iloc[-1][self.experiment.__name__]
 
     def normalize(self, points):
         ''' Normalize a point to (0,1) according to the optimizer bounds.
--- optimistic/algorithms/grid_search.py
+++ optimistic/algorithms/grid_search.py
@@ -1,6 +1,8 @@
 from optimistic.algorithms import Algorithm
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import attr
+from tqdm import tqdm_notebook as tqdm
 
 @attr.s
 class GridSearch(Algorithm):
@@ -10,12 +12,19 @@ class GridSearch(Algorithm):
     def run(self):
         dim = len(self.parameters)
         grid = []
-        for n in range(dim):
-            grid.append(np.linspace(0, 1, self.steps))
+
+        for name, parameter in self.parameters.items():
+            if name in self.points:
+                scaler = MinMaxScaler()
+                bounds = np.array(self.bounds[name])
+                points = np.array(self.points[name])
+                scaler.fit(bounds.reshape(-1, 1))
+                normalized_points = scaler.transform(points.reshape(-1, 1)).flatten()
+                grid.append(normalized_points)
+            else:
+                grid.append(np.linspace(0, 1, self.steps))
+
         points = np.transpose(np.meshgrid(*[grid[n] for n in range(dim)])).reshape(-1, dim)
 
-        for point in points:
+        for point in tqdm(points):
             self.measure(point)
-
-        best_point = points[self.data['result'].idxmin()]
-        self.actuate(best_point)
