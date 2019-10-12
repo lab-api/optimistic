57246c383db871f4b158a7f1ed2e9d41e4aa024f
Robert Fasano <robert.j.fasano@colorado.edu>
2019-10-12 09:09:08 -0600

    Add class docstring

--- optimistic/pipeline/loop.py
+++ optimistic/pipeline/loop.py
@@ -1,11 +1,17 @@
-
 import attr
 from optimistic.pipeline import Pipeline
 
 @attr.s
 class Loop(Pipeline):
+    ''' The Loop block is a subpipeline which can repeatedly execute a sequence
+        of blocks which are added to it. For example, this could be used to
+        implement a closed-loop learning cycle as follows:
+        1. Fit a response surface to acquired data
+        2. Numerically optimize to suggest the next experimental point
+        3. Measure the suggested point, add to the dataset, and repeat from step 1
+    '''
     loops = attr.ib(default=1, converter=int)
 
     def run(self):
         for i in range(self.loops):
-            Pipeline.run(self) 
+            Pipeline.run(self)
