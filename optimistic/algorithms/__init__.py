f36d7260dc2fc158740c9ac7092609cb833bdf21
Robert Fasano <robert.j.fasano@colorado.edu>
2019-12-16 17:12:29 -0700

    Combine GridSearch and ParallelGridSearch into a single optimizer

--- optimistic/algorithms/__init__.py
+++ optimistic/algorithms/__init__.py
@@ -1,4 +1,3 @@
 from .algorithm import Algorithm
 from .grid_search import GridSearch
 from .gradient_descent import GradientDescent
-from .parallel_grid_search import ParallelGridSearch
--- optimistic/algorithms/grid_search.py
+++ optimistic/algorithms/grid_search.py
@@ -2,31 +2,52 @@ from optimistic.algorithms import Algorithm
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import attr
+import pandas as pd
+import dask
+import multiprocessing as mp
+from dask.distributed import Client
 
 @attr.s
 class GridSearch(Algorithm):
     steps = attr.ib(default=20, converter=int)
     scans = attr.ib(default=1, converter=int)
+    parallel = attr.ib(default=False, converter=bool)
+    threads_per_worker = attr.ib(default=1, converter=int)
+    workers = attr.ib(default=mp.cpu_count(), converter=int)
 
-    def run(self):
+    def generate_grid(self):
         dim = len(self.parameters)
         grid = []
-
         for name, parameter in self.parameters.items():
             if name in self.points:
-                scaler = MinMaxScaler()
-                bounds = np.array(self.bounds[name])
-                points = np.array(self.points[name])
-                scaler.fit(bounds.reshape(-1, 1))
-                normalized_points = scaler.transform(points.reshape(-1, 1)).flatten()
-                grid.append(normalized_points)
+                grid.append(self.points[name])
             else:
-                grid.append(np.linspace(0, 1, self.steps))
+                grid.append(np.linspace(self.bounds[name][0], self.bounds[name][1], self.steps))
+        return np.transpose(np.meshgrid(*[grid[n] for n in range(dim)])).reshape(-1, dim)
 
-        points = np.transpose(np.meshgrid(*[grid[n] for n in range(dim)])).reshape(-1, dim)
-
-        for point in self.iterate(points):
-            self.measure(point)
-
+    def run(self):
+        if self.parallel:
+            self.run_parallel()
+        else:
+            self.run_sequential()
         best_point = self.data.loc[self.data[self.experiment.__name__].idxmin()][self.parameters].values
-        self.measure(self.normalize(best_point))
+        self.measure(best_point, normalized=False)
+
+    def run_sequential(self):
+        points = self.generate_grid()
+        for point in self.iterate(points):
+            self.measure(point, normalized=False)
+
+    def run_parallel(self):
+        points = self.generate_grid()
+        points = pd.DataFrame(points, columns = list(self.parameters.keys()))
+        client = Client(threads_per_worker=self.threads_per_worker, n_workers=self.workers)
+        display(client)
+        futures = []
+        for values in points.values:
+            future = client.submit(self.experiment, parallel=True, **dict(zip(self.parameters, values)))
+            futures.append(future)
+        results = client.gather(futures)
+
+        points[self.experiment.__name__] = results
+        self.data = points
